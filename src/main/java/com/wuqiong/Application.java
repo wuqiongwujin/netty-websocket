215b498820832750daa6d020c5f19e0615ec503b
wuqiong <872271631@qq.com>
2022-10-14 17:51:58 +0800

    初始化spring boot web

--- src/main/java/com/wuqiong/Application.java
+++ src/main/java/com/wuqiong/Application.java
@@ -1,9 +1,18 @@
 package com.wuqiong;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description
  * @Author Cain
  * @date 2022/10/14
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("spring boot web 服务启动成功");
+    }
 }
